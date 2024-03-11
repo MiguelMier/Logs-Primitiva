b1f1240b609e6ce530623c030082337f9ac9621a
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-11 12:52:24 +0100

    commit inicial primitiva

---
+++ src/main/java/es/babel/primitiva/PrimitivaApplication.java
@@ -0,0 +1,13 @@
+package es.babel.primitiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimitivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimitivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/babel/primitiva/PrimitivaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.babel.primitiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimitivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
